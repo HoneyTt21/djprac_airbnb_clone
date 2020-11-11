335f16d9e5836c6d49af3d437ccc9daf5954c131
Minjun Kim <honeytt21@kaist.ac.kr>
2020-11-12 05:10:48 +0900

    Room

---
+++ users/admin.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from . import models
+
+# admin.site.register(models.User, CustomUserAdmin)
+@admin.register(models.User)
+class CustomUserAdmin(UserAdmin):
+
+    """Custom User Admin"""
+
+    # fieldsets
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Profiles",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "birthdate",
+                    "language",
+                    "currency",
+                )
+            },
+        ),
+    )
