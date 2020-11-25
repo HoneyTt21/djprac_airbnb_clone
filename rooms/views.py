ac73f7bd251774e8b131205c9522cf58ee0e1ea9
Minjun Kim <honeytt21@kaist.ac.kr>
2020-11-26 08:05:40 +0900

    before searchview

--- rooms/views.py
+++ rooms/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, redirect
-from django.views.generic import ListView
-from django.urls import reverse
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.http import Http404
 from . import models
 
 
@@ -13,9 +13,16 @@ class HomeView(ListView):
     ordering = "created"
 
 
-def room_detail(request, pk):
-    try:
-        room = models.Room.objects.get(pk=pk)
-        return render(request, "rooms/detail.html", {"room": room})
-    except models.Room.DoesNotExist:
-        return redirect(reverse("core:home"))
+# def room_detail(request, pk):
+#     try:
+#         room = models.Room.objects.get(pk=pk)
+#         return render(request, "rooms/room_detail.html", {"room": room})
+#     except models.Room.DoesNotExist:
+#         raise Http404()
+
+
+class RoomDetail(DetailView):
+
+    """Room Detail View"""
+
+    model = models.Room
